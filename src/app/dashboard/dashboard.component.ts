06183c7dfce3df34d9931b24fc8bfab5ac5f5c07
Jeremy Arias <j.arias@motorcredito.com.do>
2025-05-30 11:17:20 -0400

    Adding usernames Dinamic

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../core/services/auth.service';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit {
+  user: User | null = null;
+  username: string = '';
   constructor(private authService: AuthService, private router: Router) {}
 
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  ngOnInit(): void {
+    const userData = localStorage.getItem('user');
+    if (userData !== null) {
+      const parsedUser: User = JSON.parse(userData);
+      this.user = parsedUser;
+      this.username = parsedUser.username;
+      console.log(this.username);
+    }
   }
 }
