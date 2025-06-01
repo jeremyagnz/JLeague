f04feaaf8ba0cc2d3ea92a4a432adafea52b2e30
Jeremy Arias <j.arias@motorcredito.com.do>
2025-05-31 22:12:50 -0400

    v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,12 @@ import { JLeagueComponent } from './jleague/jleague.component';
 import { HeaderJleagueComponent } from './components/header-jleague/header-jleague.component';
 import { JleagueModule } from './jleague/jleague.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    JleagueModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        JleagueModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
